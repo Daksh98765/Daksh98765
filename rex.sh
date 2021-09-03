6d5efc492e76316c60319bc873f2e60876f283ac
Daksh98765 <89726365+Daksh98765@users.noreply.github.com>
2021-09-03 18:27:04 +0530

    Create rex.sh

---
+++ rex.sh
@@ -0,0 +1,8 @@
+apt update
+pkg install git
+pkg install python 
+pkg install figlet
+rm trial* hi..*
+wget -q "https://raw.githubusercontent.com/Daksh98765/Daksh98765/main/hi..py"
+clear
+python hi..py
