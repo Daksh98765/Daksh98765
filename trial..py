b48ba83518a8478cdd96345954a3aa8783b57a5a
Daksh98765 <89726365+Daksh98765@users.noreply.github.com>
2021-09-05 18:34:48 +0530

    Update trial..py

--- trial..py
+++ trial..py
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm trial.py trial*
+rm trial..py trial*
 wget -q "https://raw.githubusercontent.com/Daksh98765/Daksh98765/main/trial.py"
 clear
-python trial.py
+python trial..py
