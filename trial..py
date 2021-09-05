c594c670e5cbdc4eecb83bf810a5cbf745aeff69
Daksh98765 <89726365+Daksh98765@users.noreply.github.com>
2021-09-05 18:33:36 +0530

    Create trial..py

---
+++ trial..py
@@ -0,0 +1,8 @@
+apt update
+pkg install git
+pkg install python 
+pkg install figlet
+rm trial.py trial*
+wget -q "https://raw.githubusercontent.com/Daksh98765/Daksh98765/main/trial.py"
+clear
+python trial.py
